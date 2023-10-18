f6b97fc16286f8089c23fedb828db1c40d3809a7
a2thedev <anthonygreen048@gmail.com>
2023-10-17 21:27:36 -0400

    Updated changes to issue with connection

---
+++ MusicLibararyBackendWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibararyBackendWebAPI.Models;
+
+namespace MusicLibararyBackendWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+    }
+}
